9abf5eaca4e9108b6b45cae476ee0ee0607d9255
flaviohfsilva <flavinhovinho2004@gmail.com>
2023-09-25 16:24:42 -0300

    Adicionando o repositório

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesComponent } from './pages.component';
-import { HomeComponent } from './home-landingPage/home.component';
+import { HomeComponent } from './cardiotech/home.component';
 import { LoginComponent } from './login/login.component';
 import { HomeIAComponent } from './home-ia/home-ia.component';
 import { PasswordRecoverComponent } from './password-recover/password-recover.component';
