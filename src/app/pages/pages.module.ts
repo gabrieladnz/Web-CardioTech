f0091cc5f3f0715a7cab9fa7f48e4309f41a9081
Gabriela Diniz <gadisa10@outlook.com>
2023-10-10 12:40:24 -0300

    desgraça

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { HomeComponent } from './cardiotech/home.component';
 import { LoginComponent } from './login/login.component';
-import { HomeIAComponent } from './painel/home-ia.component';
+import { HomeIAComponent } from './painel IA/home-ia.component';
 import { PasswordRecoverComponent } from './password-recover/password-recover.component';
 import { PageRoutingModule } from './pages-routing-module';
 import { NavbarModule } from '../components/navbar/navbar.module';
 import { FooterModule } from "../components/footer/footer.module";
-
+import { NavbarPainelComponent } from '../components/navbar-painel/navbar-painel.component';
+import { SidebarPainelComponent } from '../components/sidebar-painel/sidebar-painel.component';
+import { ModalIAComponent } from '../components/modal-ia/modal-ia.component';
 
 
 @NgModule({
@@ -17,7 +19,10 @@ import { FooterModule } from "../components/footer/footer.module";
         PagesComponent,
         HomeComponent,
         HomeIAComponent,
-        PasswordRecoverComponent
+        PasswordRecoverComponent,
+        NavbarPainelComponent,
+        SidebarPainelComponent,
+        ModalIAComponent
     ],
     imports: [
         CommonModule,
